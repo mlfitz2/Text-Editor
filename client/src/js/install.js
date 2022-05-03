41d2cd889418f06b46dd32a52eaecb227f3d4981
Mike Fitzpatrick <mlfitz2@gmail.com>
2022-05-02 18:56:04 -0700

    updating comments

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -9,26 +9,25 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
     // Remove the hidden class from the button.
     butInstall.classList.toggle('hidden', false);
-  });
-
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    const promptEvent = window.deferredPrompt;
+      
+  const promptEvent = window.deferredPrompt;
 
-    if (!promptEvent) {
-     return;
-    }
+  if (!promptEvent) {
+   return;
+  }
+
+  // Show prompt
+  promptEvent.prompt();
   
-    // Show prompt
-    promptEvent.prompt();
-    
-    // Reset the deferred prompt variable, it can only be used once.
-    window.deferredPrompt = null;
-    
-    butInstall.classList.toggle('hidden', true);
-  });
+  // Reset the deferred prompt variable, it can only be used once.
+  window.deferredPrompt = null;
   
+  butInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
